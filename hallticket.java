2f3afb794c249639e4b2c1f5ca9cb70289df5698
mandavillisai <saimandavilli1005@gmail.com>
2025-05-01 13:54:56 +0530

    Add files via upload

---
+++ hallticket.java
@@ -0,0 +1,23 @@
+/* This is my program written by developer */
+
+public class hallticket
+ {
+    public static void main(String[] args)
+ {
+        // Declare and assign a String variable
+        String name = "SAI";
+        String center ="Aditya College";
+        String subject ="English- 12-04-2025";
+
+        // Declare and assign an int variable
+        int hallticketnumber = 8520;
+        int age = 25;
+
+        // Print the values
+        System.out.println("Name: " + name);
+        System.out.println("Hallticket number: " + hallticketnumber);
+        System.out.println("Center: " + center);
+        System.out.println("Subject: " + subject);
+        System.out.println("Age: " + age);
+    }
+}
